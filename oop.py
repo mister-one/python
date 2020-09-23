6f1d97604ff62ebe55aff6349df1752d89a4c587
mister-one <alexandros.rea@gmail.com>
2020-09-23 18:19:18 +0100

    last update

--- oop.py
+++ oop.py
@@ -1,7 +1,18 @@
 # python Object oriented programming
 
-class employee:
+class Employee:
+    class_var = 105
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
+
+    def fullname(self):
+        print(f'{self.first} {self.last}')
+
+
+
+e_1 = Employee('corey','scha',12)
+
+
+print(e_1.__dict__)
